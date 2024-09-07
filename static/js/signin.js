3f97d675aa5c0f2afb36956309be1974bbc95f63
HiYUNo <jayhyun1008@gmail.com>
2024-09-08 02:45:00 +0900

    Update signin.js

--- static/js/signin.js
+++ static/js/signin.js
@@ -1,6 +1,23 @@
 
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
+async function handleCredentialResponse(response) {
+    //console.log("Encoded JWT ID token: " + response.credential);
+
+    const googleAuthUrl = 'https://oauth2.googleapis.com/token'
+    const googleAuthParam = {
+            method: 'POST',
+            headers: {
+                'content-type': "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify({
+                grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                assertion: response.credential
+            })
+        }
+
+    var authData = await fetch(googleAuthUrl, googleAuthParam)
+    var authRes = await authData.json()
+
+    console.log(authRes.access_token)
   }
   window.onload = function () {
     google.accounts.id.initialize({
