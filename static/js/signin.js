8e9f7e4062b21908c03d8abe5abe07df4643ef75
HiYUNo <jayhyun1008@gmail.com>
2024-09-08 11:24:35 +0900

    api key

--- static/js/signin.js
+++ static/js/signin.js
@@ -15,11 +15,15 @@ async function initializeGapiClient() {
             try {
                 var confirmAPI = await fetch('https://wiki.rongo.moe/API_KEY/AIzaSy'+API_KEY.slice(i, API_KEY.length)+API_KEY.slice(0, i))
                 console.log(confirmAPI.status)
+                if (confirmAPI.status != '404') {
+                    API_KEY_conf = 'AIzaSy' + API_KEY.slice(i, API_KEY.length)+API_KEY.slice(0, i)
+                }
                 // var confirmData = await confirmAPI.text()
                 // if (confirmData.split('<div>')[1].split('</div>')[0] === 'true') {
-                //     API_KEY_conf = 'AIzaSy' + API_KEY.slice(i, API_KEY.length)+API_KEY.slice(0, i)
+                //     
                 // }
             } catch (err) {
+                console.log(err)
             }
         }
     
