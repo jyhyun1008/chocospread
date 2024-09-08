e85af1c571939e3a546864a4c5bc46d7e855dbec
HiYUNo <jayhyun1008@gmail.com>
2024-09-08 17:18:00 +0900

    extract id from email

--- pages/_title/index.vue
+++ pages/_title/index.vue
@@ -35,7 +35,7 @@ export default {
         return {
             script: [
                 { src: 'https://cdn.jsdelivr.net/npm/marked/marked.min.js', defer: true },
-                { src: '../js/index.js?v=8', defer: true },
+                { src: '../js/index.js?v=9', defer: true },
             ],
             link: [
                 { href: 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css', rel: 'stylesheet'},
