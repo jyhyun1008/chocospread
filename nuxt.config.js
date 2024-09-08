a46eef981bf122abd26fbc0a4f47675c6c3d544d
HiYUNo <jayhyun1008@gmail.com>
2024-09-08 10:03:42 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ export default {
       for (let i=0; i<wikiListArray.length; i++) {
           wikiList.push('/'+encodeURIComponent(wikiListArray[i].properties.title))
       }
-      const API_KEY = process.env.API_KEY.split('AIzaSy')[1];
+      var API_KEY = process.env.API_KEY.split('AIzaSy')[1];
   
       for (let i=0;i< API_KEY.length;i++) {
           wikiList.push('/API_KEY/AIzaSy'+API_KEY.slice(i, API_KEY.length)+API_KEY.slice(0, i))
